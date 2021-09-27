46781d80b71bc8265c42008b6c736840c6bc4679
Mor Codou GUEYE <morcodou@gmail.com>
2021-09-27 02:26:59 -0400

    setup and configure Auth0, add graphQL server with ApolloClient

---
+++ utils/auth0.ts
@@ -0,0 +1,20 @@
+import { initAuth0 } from '@auth0/nextjs-auth0';
+import getConfig from 'next/config';
+
+const { serverRuntimeConfig } = getConfig();
+const { auth, cookieSecret } = serverRuntimeConfig;
+
+export default initAuth0({
+  ...auth,
+  session: {
+    cookieSecret,
+    cookielifetime: 60 * 60 * 8,
+    storeIdToken: false,
+    storeAccessToken: false,
+    storeRefreshToken: false,
+  },
+  oidcClient: {
+    httpTimeout: 2500,
+    clockTolerence: 10000,
+  }
+})
