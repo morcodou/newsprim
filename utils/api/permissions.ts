0e5a1029165de3884cdd6cc3236fc9c77a7ebc5e
Mor Codou GUEYE <morcodou@gmail.com>
2021-09-30 09:18:42 -0400

    Add queries, mutations and fragments

--- utils/api/permissions.ts
+++ utils/api/permissions.ts
@@ -2,14 +2,14 @@ import { shield, rule } from "graphql-shield";
 import * as _ from 'lodash';
 
 const rules = {
-    isAuthenticated: rule()(async (_parent, _args, context) => {
-        // return _.isEmpty(context.user) ? false : true
-        return _.isEmpty(context.user) ? true : true
-    })
+    isAuthenticated: rule()
+        (
+            (_parent, _args, context) => _.isEmpty(context.user) ? false : true
+        )
 }
 
 export const permissions = shield({
     Query: {
-        // hello: rules.isAuthenticated,
+        hello: rules.isAuthenticated,
     }
 })
